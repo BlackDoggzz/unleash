451856e610be9177901b5fef2ad9069bb2abdc56
Gastón Fournier <gaston@getunleash.io>
2023-02-21 15:18:06 +0100

    Handle both email and username as undefined

--- src/lib/util/extract-user.ts
+++ src/lib/util/extract-user.ts
@@ -1,7 +1,13 @@
 import { IAuthRequest, IUser } from '../server-impl';
 
 export function extractUsernameFromUser(user: IUser): string {
-    return user ? user.email || user.username : 'unknown';
+    if (user) {
+        const candidate = user.email || user.username;
+        if (candidate) {
+            return candidate;
+        }
+    }
+    return 'unknown';
 }
 
 export function extractUsername(req: IAuthRequest): string {
